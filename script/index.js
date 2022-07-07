d7ebe63ee7d49e6edf48410f1202cb1f2412d0ff
Alkaidcc <932214806@qq.com>
2022-07-07 16:20:13 +0800

    feat: add picture export

--- script/index.js
+++ script/index.js
@@ -15,10 +15,13 @@ const body = document.querySelector('body');
 const quote_text = document.querySelector('.quote-text');
 const quote_author = document.querySelector('.quote-author');
 const buttons = document.querySelectorAll('.button');
+const export_button = document.querySelector('#export');
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
 /**
  * @description - This function is used to get a random color from the colors array
  */
-changeColor = () => {
+const changeColor = () => {
   let color = colors[Math.floor(Math.random() * colors.length)];
   body.style.backgroundColor = color;
   quote_text.style.color = color;
@@ -26,13 +29,15 @@ changeColor = () => {
   for (let button of buttons) {
     button.style.backgroundColor = color;
   }
+  container.style.backgroundColor = color;
 }
+
 
 async function get(url=baseURL) {
   const response = await fetch(url)
   return response.json()
 }
-getQuote = () => {
+const getQuote = () => {
   get(baseURL).then(data => {
     quote_text.innerHTML = data.hitokoto
     quote_author.innerHTML = data.from
@@ -43,4 +48,23 @@ new_quote.addEventListener('click', () => {
   changeColor();
   getQuote();
 })
+export_button.addEventListener('click', () => {
+  let computed_height = document.querySelector('.card').offsetHeight + 40;
+  const options = {
+    width: 520,
+    height: computed_height
+  }
+  domtoimage.toJpeg(document.querySelector('.container'),options)
+    .then(dataUrl => {
+      const link = document.createElement('a');
+      link.download = '一言.jpeg';
+      link.href = dataUrl;
+      link.click();
+    }).catch(error => {
+      console.error('oops! something went wrong!', error);
+    })
+
+})
+
+
 window.onload = getQuote;
